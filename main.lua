7e358bb98a0c863d7ed163d06330856ef3303d27
wewewer4fteeeeeee <151419080+wewewer4fteeeeeee@users.noreply.github.com>
2026-05-10 17:49:15 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ local TweenService = game:GetService("TweenService")
 
 local HttpService = game:GetService("HttpService")
 local SetClipboard = setclipboard or print
-local BaseUrl = "https://highways-invitations-concerning-shirt.trycloudflare.com"
+local BaseUrl = "https://ordinary-buffalo-postage-yang.trycloudflare.com"
 
 function main:Begin(PROPS) 
     if not PROPS then return end
