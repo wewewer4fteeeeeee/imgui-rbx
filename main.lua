73709c280e385c3e9c5e9000ad942357352952c6
wewewer4fteeeeeee <151419080+wewewer4fteeeeeee@users.noreply.github.com>
2026-06-03 00:53:28 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ local TweenService = game:GetService("TweenService")
 
 local HttpService = game:GetService("HttpService")
 local SetClipboard = setclipboard or print
-local BaseUrl = "https://absolute-camcorders-which-chairs.trycloudflare.com"
+local BaseUrl = "https://private-humidity-exactly-throwing.trycloudflare.com"
 
 function main:Begin(PROPS) 
     if not PROPS then return end
