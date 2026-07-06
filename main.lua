58bdeb79aa3ab7d92b63d7d8ff7a191e26737066
wewewer4fteeeeeee <151419080+wewewer4fteeeeeee@users.noreply.github.com>
2026-07-06 10:43:23 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ function util:egg(stringx)
         for j = 1, math.random(5, 10) do
             egged = egged .. string.char(math.random(32, 126))
         end
-        output[#output+1] = string.format("%03d%s", charCode, egged)f
+        output[#output+1] = string.format("%03d%s", charCode, egged)
     end
     return table.concat(output)
 end
